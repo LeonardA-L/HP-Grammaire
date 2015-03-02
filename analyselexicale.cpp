33cd0f6f2257b421e16fed7b478df4983cfcd4af
LeonardA-L <leonard.allain-launay@insa-lyon.fr>
2015-03-02 09:31:51 +0100

    Adding regexes

--- analyselexicale.cpp
+++ analyselexicale.cpp
@@ -5,12 +5,30 @@
 using namespace std;
 using namespace boost;
 
+// Keywords
+regex re_const("const");
+regex re_var("var");
+regex re_lire("lire");
+regex re_ecrire("ecrire");
+// Symbols
+regex re_oppA("(\+|-)");
+regex re_oppM("(*|/)");
+regex re_equals("=");
+regex re_affects(":=");
+// Terminal
+regex re_identifier("(^(?!const|var|lire|ecrire)(\w+)|^\w+(const|var|lire|ecrire)|(const|var|lire|ecrire)\w+)");
+regex re_numeral("(\d+)");
+
 void checkRegexMatch(string s, regex re){
 	try
 	{
 	   if (!regex_match(s, re))
 	   {
 		  throw "The string doesn't match the regex";
+	   }
+	   else
+	   {
+		  throw "The string matches the regex";
 	   }
 	}
 	catch (regex_error& e)
@@ -20,23 +38,32 @@ void checkRegexMatch(string s, regex re){
 	}
 	catch (char const* msg)
 	{
-		cout << msg << endl;
+		cerr << msg << endl;
 	}
 }
 
 void analyse()
 {
-	//char c;
-	//string buff("");
-	/*while(cin >> c)
-	{*/
-	//buff += c;
-	regex re("ok");
-	string a("ok");
-	string b("12");
-	checkRegexMatch(a,re);
-	checkRegexMatch(b,re);
-	//}
+	char c;
+	string buff("");
+	while(cin >> c)
+	{
+		switch(c)
+		{
+			case ' ':
+			continue;
+			break;
+		}
+		buff += c;
+		
+		/*
+		regex re("ok");
+		string a("ok");
+		string b("12");
+		checkRegexMatch(a,re);
+		checkRegexMatch(b,re);
+		*/
+	}
 }
 
 
