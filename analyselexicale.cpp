44c5c5f7dfb20cd36e5d27c0b80b2fcb122bc568
LeonardA-L <leonard.allain-launay@insa-lyon.fr>
2015-03-02 08:38:39 +0100

    Reading from cin. Initial commit

---
+++ analyselexicale.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+
+int main()
+{
+	cout << "coucou" << endl;
+	char c;
+	while(cin >> c)
+	{
+		cout << c;
+	}
+	cout << endl;
+	return 0;
+}
