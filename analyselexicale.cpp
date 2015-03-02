8d9c879e20acf0453a890fcc290cdd2c1f401863
LeonardA-L <leonard.allain-launay@insa-lyon.fr>
2015-03-02 09:12:24 +0100

    Matching regexes

--- analyselexicale.cpp
+++ analyselexicale.cpp
@@ -1,17 +1,48 @@
 #include <iostream>
 #include <string> 
+#include <boost/regex.hpp>
 
 using namespace std;
+using namespace boost;
+
+void checkRegexMatch(string s, regex re){
+	try
+	{
+	   if (!regex_match(s, re))
+	   {
+		  throw "The string doesn't match the regex";
+	   }
+	}
+	catch (regex_error& e)
+	{
+	   cerr << "The regexp " << re << " is invalid!" << endl;
+	   throw(e);
+	}
+	catch (char const* msg)
+	{
+		cout << msg << endl;
+	}
+}
+
+void analyse()
+{
+	//char c;
+	//string buff("");
+	/*while(cin >> c)
+	{*/
+	//buff += c;
+	regex re("ok");
+	string a("ok");
+	string b("12");
+	checkRegexMatch(a,re);
+	checkRegexMatch(b,re);
+	//}
+}
 
 
 int main()
 {
 	cout << "coucou" << endl;
-	char c;
-	while(cin >> c)
-	{
-		cout << c;
-	}
-	cout << endl;
+	analyse();
 	return 0;
 }
