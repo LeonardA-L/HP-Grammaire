14992d62d03bc7cd07956f7cf6aebdcd8b229902
Minjard Segolene <sminjard@if501-219-22.insa-lyon.fr>
2015-03-16 10:20:59 +0100

    Etats 33, 34 et 35

---
+++ Etat_33.cpp
@@ -0,0 +1,65 @@
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+
+//------------------------------------------------------ Include personnel
+#include "Etat_33.h"
+#include "Etat_34.h"
+
+
+//------------------------------------------------------------- Constantes
+
+//---------------------------------------------------- Variables de classe
+
+//----------------------------------------------------------- Types privés
+
+
+//----------------------------------------------------------------- PUBLIC
+//-------------------------------------------------------- Fonctions amies
+
+//----------------------------------------------------- Méthodes publiques
+
+bool Etat_33::transition ( Automate & a, Symbole * s )
+{
+	switch(*s)
+	{
+		case(DOUBLE_POINT_EGAL) :
+			Etat_34 *etat34=new Etat_34();
+			a.decalage(s,etat34);
+			break;
+		default : 
+			return false;
+	}
+	return true;
+} 
+
+//------------------------------------------------- Surcharge d'opérateurs
+
+
+
+//-------------------------------------------- Constructeurs - destructeur
+Etat_33::Etat_33 ( )
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <Etat_0>" << endl;
+#endif
+    _state_num = 0;
+}
+
+
+Etat_33::~Etat_33 ( )
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <Etat_0>" << endl;
+#endif
+}
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
+
+
+//------------------------------------------------------- Méthodes privées
---
+++ Etat_35.cpp
@@ -0,0 +1,74 @@
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+
+//------------------------------------------------------ Include personnel
+#include "Etat_35.h"
+#include "Etat_36.h"
+#include "Etat_30.h"
+#include "Etat_31.h"
+
+//------------------------------------------------------------- Constantes
+
+//---------------------------------------------------- Variables de classe
+
+//----------------------------------------------------------- Types privés
+
+
+//----------------------------------------------------------------- PUBLIC
+//-------------------------------------------------------- Fonctions amies
+
+//----------------------------------------------------- Méthodes publiques
+
+bool Etat_35::transition ( Automate & a, Symbole * s )
+{
+	switch(*s)
+	{
+		case(POINT_VIRGULE) :
+			Etat_36 *etat36=new Etat_36();
+			a.decalage(s,etat36);
+			break;
+		case(PLUS) :
+			Etat_30 *etat30=new Etat_30();
+			a.decalage(s, etat30);
+			break;
+		case(MOINS) :
+			Etat_31 *etat31=new Etat_31();
+			a.decalage(s, etat31);
+			break;	
+		default : 
+			return false;
+	}
+	return true;
+} 
+
+//------------------------------------------------- Surcharge d'opérateurs
+
+
+
+//-------------------------------------------- Constructeurs - destructeur
+Etat_35::Etat_35 ( )
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <Etat_0>" << endl;
+#endif
+    _state_num = 0;
+}
+
+
+Etat_35::~Etat_35 ( )
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <Etat_0>" << endl;
+#endif
+}
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
+
+
+//------------------------------------------------------- Méthodes privées
