45c84572007f8baa598f01a2204e9d1414ab975a
BenjaminLegrand <benjamin.legrand93@gmail.com>
2015-03-16 08:49:57 +0100

    code Etat 0

--- Etat_0.cpp
+++ Etat_0.cpp
@@ -21,11 +21,26 @@ using namespace std;
 
 bool Etat_0::transition ( Automate & a, Symbole * s )
 {
-	switch(*s)
+	if(*s >= 100)
 	{
-		case(0) :
-			break;
+		//Symboles non terminaux
+		switch(*s)
+		{
+			case(P) :
+				a.decalage(s, new Etat_1());
+				break;
+			case(DEC) :
+				a.decalage(s, new Etat_2());
+				break;
+			default :
+				break;
+		}
+	}else
+	{
+		//Symboles terminaux
+		a.reduction(0, new Dec(), new Etat_2());
 	}
+	
 } 
 
 //------------------------------------------------- Surcharge d'opérateurs
