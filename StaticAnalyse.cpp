d4935a7af667ed7394743109c90e60756f254fdb
Mathieu THoretton <mathieu@thoretton.com>
2015-03-28 18:41:21 +0100

    pass test 06.3 and 06.4

--- StaticAnalyse.cpp
+++ StaticAnalyse.cpp
@@ -16,35 +16,38 @@ bool staticAnalyse(vector<Symbole*> & symbol_table){
 			stringstream errors;
 			
 			string type("variable");
-			if(id->isVar()&&!id->isVar()){ // variable by default (to pass test 06.4)
+			if(id->isVar()&&!id->isVar()){ // need to be variable by default (to pass test 06.4)
 				type = "constante";
 			}
 			
 			
-			if(!id->isDeclared()){
+			if(!id->isDeclared()){ // if is not declared we have to show only this error on stderr (to pass test 06.3)
 				idFail = true;
 				errors << "\t La " << type << " n'est pas déclarée."<<endl;
 			}
-			else if(id->isVar() && !id->isAssigned()){
-				idFail = true;
-				errors << "\t La " << type << " n'a pas de valeur assignée." << endl;
-			}
-			else if(!id->isVar() && id->isAssigned()){
-				errors << "\t Une constante ne peut être modifiée." << endl;
-			}
-			else if(!id->isUsed()){
-				idFail = true;
-				errors << "\t La " << type << " n'est pas utilisée." << endl;
-			}
-			/* Impossible de l'obtenir pour le moment
-			if(id->isAssignedBeforeDeclared()){
-				idFail = true;
-				errors << "\t La " << type << " est assignée avant d'être déclarée." << endl;
-			}
-			*/
-			else if(id->isUsedBeforeAssigned()){
-				idFail = true;
-				errors << "\t La " << type << " est utilisée avant d'avoir reçu une valeur." << endl;
+			else
+			{
+				if(id->isVar() && !id->isAssigned()){
+					idFail = true;
+					errors << "\t La " << type << " n'a pas de valeur assignée." << endl;
+				}
+				if(!id->isVar() && id->isAssigned()){
+					errors << "\t Une constante ne peut être modifiée." << endl;
+				}
+				if(!id->isUsed()){
+					idFail = true;
+					errors << "\t La " << type << " n'est pas utilisée." << endl;
+				}
+				/* Impossible de l'obtenir pour le moment
+				if(id->isAssignedBeforeDeclared()){
+					idFail = true;
+					errors << "\t La " << type << " est assignée avant d'être déclarée." << endl;
+				}
+				*/
+				if(id->isUsedBeforeAssigned()){
+					idFail = true;
+					errors << "\t La " << type << " est utilisée avant d'avoir reçu une valeur." << endl;
+				}
 			}
 			
 			
